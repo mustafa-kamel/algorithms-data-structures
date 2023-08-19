9deeaed51b2138dbf865aa3bce3b4b9ba33ccf15
mustafa-kamel <mostafak252@gmail.com>
2023-08-19 23:40:16 +0300

    Add tests for recursion

---
+++ tests/__init__.py
@@ -0,0 +1,10 @@
+from .test_recursion import TestCountDown
+from .test_searching import TestBinarySearch
+from .test_sorting import TestFindSmallestItem, TestSelectionSort
+
+__all__ = [
+    "TestCountDown",
+    "TestBinarySearch",
+    "TestFindSmallestItem",
+    "TestSelectionSort",
+]
---
+++ tests/test_recursion/__init__.py
@@ -0,0 +1,7 @@
+from .test_array_count import TestArrayCount
+from .test_array_sum import TestArraySum
+from .test_binary_search_recursively import TestBinarySearch
+from .test_count_down import TestCountDown
+from .test_find_max_num_in_array import TestFindMaxNumInArray
+
+__all__ = ["TestArrayCount", "TestArraySum", "TestBinarySearch", "TestCountDown", "TestFindMaxNumInArray"]
