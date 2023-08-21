29859b5e138d279331b7236ce7e49582893b55d0
Mustafa Kamel <mostafak252@gmail.com>
2023-08-21 23:20:13 +0000

    Refine code

--- recursion/recursive.py
+++ recursion/recursive.py
@@ -11,22 +11,19 @@ def count_down(high: int) -> None:
 def find_max_num_in_array(arr: list) -> int:
     if not arr:
         return 0
-    return max(arr.pop(), find_max_num_in_array(arr))
+    return max(arr[0], find_max_num_in_array(arr[1:]))
 
 
 def array_count(arr: list) -> int:
-    count = 0
     if not arr:
         return 0
-    arr.pop()
-    count += 1
-    return count + array_count(arr)
+    return 1 + array_count(arr[1:])
 
 
 def array_sum(arr: list) -> int:
     if not arr:
         return 0
-    return arr.pop() + array_sum(arr)
+    return arr[0] + array_sum(arr[1:])
 
 
 def binary_search(needle: int, haystack: Union[tuple, list, set]) -> Union[int, None]:
